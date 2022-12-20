bcaa10f6521b7e1c3696ca922ad8947e93ca1037
Ammar Zafar <ammarzafar128@gmail.com>
2022-12-20 13:43:59 -0800

    created readData to read in csv file but directory is incorrect

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        readData File = new readData();
+        File.readFile();
+        //System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/org/example/readData.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class readData {
+    public void readFile() {
+        Scanner scan = null;
+        try {
+            File myObj = new File("/data/WorldCupGoals.csv");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if( data != null){
+                    String tokens[] = data.split(",");
+                }
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
+
